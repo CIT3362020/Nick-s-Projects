540acbe6890c6c73de89decaff744f156bab408f
Nick Louisma <60583235+nicklouisma@users.noreply.github.com>
2020-05-14 21:51:34 -0400

    Add files via upload

---
+++ Lesson2/home.js
@@ -0,0 +1,12 @@
+$(window).on("load",function(){
+     $(".loader-wrapper").fadeOut(4000);
+});
+
+var modal = document.getElementById('id01');
+var modal = document.getElementById('id02');
+var modal = document.getElementById('id03');
+var modal = document.getElementById('id04');
+
+
+// When the user clicks anywhere outside of the modal, close it
+
